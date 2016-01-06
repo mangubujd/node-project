6d7a90a7ea1a66ac33882e8388ee145a8aba64a2
Joan Ortega <jomaora@gmail.com>
2016-01-06 18:31:58 +0100

    (third part): Adding signup route and user schema to create users
    Adding bcrypt lib to hash password saved in db

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var methodOverride = require('method-override');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var songs = require('./routes/songs');
+var signup = require('./routes/signup');
 var database = require('./database');
 
 var app = express();
@@ -38,6 +39,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/songs', songs);
+app.use('/signup', signup);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
